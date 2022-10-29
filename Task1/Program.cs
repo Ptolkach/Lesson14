f42ba35a29c69e5aa780b70fc867c5c3acab6d55
Pusher <Pusher@DESKTOP-9SFS5QH>
2022-10-29 18:54:49 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cat cat = new Cat("Олег");
+            cat.ShowInfo();
+            Dog dog = new Dog("Полкан");
+            dog.ShowInfo();
+            Console.ReadKey();
+            
+
+        }
+    }
+}
